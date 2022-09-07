70e936781d2c21e17d3a2e9ec9134aabc23c51bf
dangquang <dangquang.nguyenhui1999@gmail.com>
2022-09-07 09:53:20 +0700

    Make lint success and run

--- website/app.js
+++ website/app.js
@@ -1,28 +1,28 @@
-const nunjucks = require('nunjucks')
-const express = require('express')
+const nunjucks = require("nunjucks");
+const express = require("express");
 
-const app = express()
+const app = express();
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   express: app,
   autoescape: false,
-  noCache: true
-})
+  noCache: true,
+});
 
-app.set('view engine', 'njk')
-app.use(express.static('public'))
+app.set("view engine", "njk");
+app.use(express.static("public"));
 
-app.locals.title = process.env.WEBSITE_TITLE || 'Parrot'
-app.locals.image = process.env.WEBSITE_IMAGE || 'parrot-1.jpg'
+app.locals.title = process.env.WEBSITE_TITLE || "Parrot";
+app.locals.image = process.env.WEBSITE_IMAGE || "parrot-1.jpg";
 app.locals.version =
-  process.env.WEBSITE_VERSION || require('./package.json').version
+  process.env.WEBSITE_VERSION || require("./package.json").version;
 
-app.get('/', async (req, res) => {
-  res.render('index')x
-})
+app.get("/", async (req, res) => {
+  res.render("index");
+});
 
-app.get('/healthcheck', (req, res) => {
-  res.json({ uptime: process.uptime() })
-})
+app.get("/healthcheck", (req, res) => {
+  res.json({ uptime: process.uptime() });
+});
 
-module.exports = app
+module.exports = app;
